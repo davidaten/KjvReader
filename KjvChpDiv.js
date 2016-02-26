a8d3947c3a06c1e102b712bcd660744b5e4ad18e
davidw <davidw@aten-ca.com>
2016-02-25 17:55:06 -0800

    2-25-0

--- KjvChpDiv.js
+++ KjvChpDiv.js
@@ -12,7 +12,8 @@ var KjvChpInit = function(idx)
 KjvChpDiv= function(bk_obj)
 {
 	var tree_l1 = new DwxUiTree();
-	var l1_num=bk_obj.ChpNumInBk/10;
+	tree_l1.ChkSignOn = 0;
+	var l1_num=parseInt(bk_obj.ChpNumInBk/10);
 	if (bk_obj.ChpNumInBk%10)
 		l1_num++;
 	//First level: old and new testaments.
@@ -23,16 +24,17 @@ KjvChpDiv= function(bk_obj)
 		if (c2>bk_obj.ChpNumInBk)
 			c2=bk_obj.ChpNumInBk;
 		tree_l1.NodeAdd(-1, sprintf("T-%s", c1), sprintf("%s~%s", c1, c2) );
-
+		tree_l1.NodeAry[l1].SubStat = 0; //Packed
+		
 		var tree_l2 = new DwxUiTree();
 		tree_l1.NodeSubSet(l1, tree_l2);
-		//tree_l2.ChkSignOn = chk_sign_on;
+		tree_l2.ChkSignOn = 0;
 		var l2_num=c2-c1+1;
 		//Each first level has 5 catelogs.
 		for (var l2 = 0; l2 < l2_num; l2++) 
 		{
-			tree_l2.NodeAdd(-1, sprintf("C-%s", c1+l2, c1+l2));
-			tree_l2.NodeAry[l2].SubStat = 0;
+			tree_l2.NodeAdd(-1, sprintf("C-%s", c1+l2), c1+l2);
+			//tree_l2.NodeAry[l2].SubStat = 1; //Packed
 		}
 	}
 	
--- MsgDat.js
+++ MsgDat.js
@@ -110,6 +110,6 @@ function MsgListDemo()
 	var Msg=new MsgList();
 	document.body.appendChild(Msg.WrapDiv);
 	var div=KjvBkDiv();
-	var bk_obj=new KjvBkInit(1);
+	var bk_obj=new KjvBkInit(3);
 	var div=KjvChpDiv(bk_obj);
 }
